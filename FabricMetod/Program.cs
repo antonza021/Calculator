5ddb4fb363c4b8d2b91f11ab395305e4344b2797
User <User@WIN-AVGURKPU8OU>
2023-12-26 23:45:35 +0700

    Добавьте файлы проекта.

---
+++ FabricMetod/Program.cs
@@ -0,0 +1,23 @@
+﻿using FabricMethod.Exeptions;
+
+namespace FabricMethod
+{
+    class Program
+    {
+        static void Main()
+        {
+            StackCalculator calculator = new StackCalculator();
+
+            try
+            {
+                double result = calculator.Calculate("10 30 10 * + 10 +");
+                Console.WriteLine($"Result: {result}");
+            }
+            catch (CalculatorException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+
+}
